2f5ccb670026a6c73e98e595c240ced577131629
Kazalev <kristian.kazalev@abv.bg>
2019-09-07 11:38:07 +0300

    Task

---
+++ Basic Syntax, Conditional Statements and Loops/08. Foreign Languages.js
@@ -0,0 +1,25 @@
+function lang(country){
+    switch (country) {
+        case "USA":
+            console.log("English");
+            break;
+        case "England":
+            console.log("English");
+            break;
+        case "Spain":
+            console.log("Spanish");
+            break;
+        case "Argentina":
+            console.log("Spanish");
+            break;
+        case "Mexico":
+        console.log("Spanish");
+        break;
+
+        default:
+        console.log("unknown");
+            break;
+    }
+}
+
+lang("England");
