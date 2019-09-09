8b3e0c8726dba077e89f0c18f284391374cdb120
Kazalev <kristian.kazalev@abv.bg>
2019-09-09 08:18:04 +0300

    Task

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/01. Ages.js
@@ -0,0 +1,16 @@
+function solve(age){
+
+    if(0 <= age && age <= 2){
+        console.log("baby");   
+    } else if (3 <= age && age <= 13){
+        console.log("child");  
+    } else if (14 <= age && age <= 19){
+        console.log("teenager");  
+    } else if (20 <= age && age <= 65){
+        console.log("adult");  
+    } else {
+        console.log("elder");  
+    }
+}
+
+solve (100);
