d7a9772b090aa02405509aa9172b0a873a2c27cb
Kazalev <kristian.kazalev@abv.bg>
2019-09-11 15:42:58 +0300

    Task

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/05. Leap Year.js
@@ -0,0 +1,13 @@
+function solve(year){
+
+    if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+        console.log("yes");
+    } else {
+        console.log("no");   
+    }
+}
+
+solve(1984)
+solve(2003)
+solve(4)
+solve(2000)
