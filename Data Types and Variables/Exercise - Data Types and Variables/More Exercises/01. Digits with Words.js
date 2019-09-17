a95fb7f7f37501f317f3de89f19665ffc21a4f48
Kazalev <kristian.kazalev@abv.bg>
2019-09-17 19:54:49 +0300

    Task

---
+++ Data Types and Variables/Exercise - Data Types and Variables/More Exercises/01. Digits with Words.js
@@ -0,0 +1,38 @@
+function solve(number){
+
+    switch(number){
+        case 'one':
+            console.log(1);
+            break;
+        case 'two':
+            console.log(2);
+            break;
+        case 'three':
+            console.log(3);
+            break;
+        case 'four':
+            console.log(4);
+            break;
+        case 'five':
+            console.log(5);
+            break;
+        case 'six':
+            console.log(6);
+            break;
+        case 'seven':
+            console.log(7);
+            break;
+        case 'eight':
+            console.log(8);
+            break;
+        case 'nine':
+            console.log(9);
+            break;
+        case 'ten':
+            console.log(10);
+            break;
+    }
+    
+}
+
+solve('four')
