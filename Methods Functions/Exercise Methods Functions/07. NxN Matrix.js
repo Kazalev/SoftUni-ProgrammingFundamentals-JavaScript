e37efae310c8cd385ed74cfe31129a3ee3c41c0a
Kazalev <kristian.kazalev@abv.bg>
2019-09-26 20:21:37 +0300

    Task

---
+++ Methods Functions/Exercise Methods Functions/07. NxN Matrix.js
@@ -0,0 +1,14 @@
+function solve(num){
+
+    let arr = '';
+    for(let i = 0; i < num; i++){
+        arr = num + ' ';
+        for(let j = num; j < i.length; j++){
+
+        }
+
+        console.log(arr.repeat(num));
+    }
+}
+
+solve(7);
